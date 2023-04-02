714d968207da5bea5fa63d7b3430150e55a16fe5
TyDennis <46075417+TatendaTy@users.noreply.github.com>
2023-04-03 00:50:35 +0300

    finished the remaining test cases as required

--- test_roman_to_int.py
+++ test_roman_to_int.py
@@ -1,37 +1,7 @@
 import unittest
 from roman_to_int import roman_to_int
 
-class RomanToIntTest(unittest.TestCase):
-    
-    def test_basic_numerals(self):
-        self.assertEqual(roman_to_int("I"), 1)
-        self.assertEqual(roman_to_int("V"), 5)
-        self.assertEqual(roman_to_int("X"), 10)
-        self.assertEqual(roman_to_int("L"), 50)
-        self.assertEqual(roman_to_int("C"), 100)
-        self.assertEqual(roman_to_int("D"), 500)
-        self.assertEqual(roman_to_int("M"), 1000)
-        
-    def test_complex_numerals(self):
-        self.assertEqual(roman_to_int("IX"), 9)
-        self.assertEqual(roman_to_int("XXIV"), 24)
-        self.assertEqual(roman_to_int("LXXXIX"), 89)
-        self.assertEqual(roman_to_int("CDXCIV"), 494)
-        self.assertEqual(roman_to_int("CMXCIX"), 999)
-        
-    def test_invalid_input(self):
-        errRes = "invalid roman numeral!"
-        self.assertEqual(roman_to_int("Q"), errRes)
-        self.assertEqual(roman_to_int("MMCDLXXXIIX"), errRes)
-        self.assertEqual(roman_to_int("ABC"), errRes)
-
-if __name__ == '__main__':
-    unittest.main()
-
-# ---------------------------------------------------------------------------
-
-## REMAINING TESTS
-
+# this code tests 
 # Single Letters:I, V, X, L, C, D, M.
 # Many Letters: XI
 # Subtractive notation (SN):IV
@@ -42,6 +12,52 @@ if __name__ == '__main__':
 # Invalid and valid Letter: XIZ
 # Not Valid:VV
 # Null:
+class RomanToIntTest(unittest.TestCase):
+    def test_single_letters(self):
+        self.assertEqual(roman_to_int('I'), 1)
+        self.assertEqual(roman_to_int('V'), 5)
+        self.assertEqual(roman_to_int('X'), 10)
+        
+    def test_many_letters(self):
+        self.assertEqual(roman_to_int('XI'), 11)
+        self.assertEqual(roman_to_int('CCC'), 300)
+        self.assertEqual(roman_to_int('MDCLXVI'), 1666)
+        
+    def test_subtractive_notation(self):
+        self.assertEqual(roman_to_int('IX'), 9)
+        self.assertEqual(roman_to_int('XL'), 40)
+        self.assertEqual(roman_to_int('CM'), 900)
+        
+    def test_with_and_without_subtractive_notation(self):
+        self.assertEqual(roman_to_int('XIV'), 14)
+        self.assertEqual(roman_to_int('XXXIX'), 39)
+        self.assertEqual(roman_to_int('CXLIV'), 144)
+        
+    def test_repetition(self):
+        self.assertEqual(roman_to_int('II'), 2)
+        self.assertEqual(roman_to_int('XXX'), 30)
+        self.assertEqual(roman_to_int('CCC'), 300)
+        
+    def test_first_number(self):
+        self.assertEqual(roman_to_int('I'), 1)
+        
+    def test_invalid_letter(self):
+        with self.assertRaises(ValueError):
+            roman_to_int('Z')
+            
+    def test_invalid_and_valid_letter(self):
+        with self.assertRaises(ValueError):
+            roman_to_int('XIZ')
+        
+    def test_not_valid(self):
+        with self.assertRaises(ValueError):
+            roman_to_int('VV')
+        
+    def test_null(self):
+        self.assertEqual(roman_to_int(''), 0)
 
+if __name__ == '__main__':
+    unittest.main()
 
+# ---------------------------------------------------------------------------
 
